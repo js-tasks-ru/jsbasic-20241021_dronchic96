4e3e1e8a4f802fd4fc6e43d31dc155e70dfe3e30
Andrey Kozhuhov <andrpre96@gmail.com>
2024-10-23 20:50:21 +0300

    1 lesson / 1 task: solved

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,13 @@
 function factorial(n) {
-  // ваш код...
+  if (!n) return 1;
+
+  let result = n;
+
+  for (let i = n; i > 1; i--) {
+    result *= i - 1;
+  }
+
+  return result;
 }
+
+console.log(factorial(5));
