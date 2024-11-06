96050520d1ffe49ec09a4052494dbfa8a71d9b1a
Andrey Kozhuhov <andrpre96@gmail.com>
2024-11-06 13:42:49 +0300

    4 lesson / 1,2,3 task: solved

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,29 @@
 function highlight(table) {
-  // ваш код...
+  for (let i = 1; i < table.rows.length; i++) {
+    let row = table.rows[i];
+    let status = row.cells[3];
+    let gender = row.cells[2];
+    let age = row.cells[1];
+
+    switch (status.dataset.available) {
+    case "true":
+      row.classList.add("available");
+      break;
+    case "false":
+      row.classList.add("unavailable");
+      break;
+    default:
+      row.setAttribute("hidden", "");
+    }
+
+    if (gender.innerText === "m") {
+      row.classList.add("male");
+    } else if (gender.innerText === "f") {
+      row.classList.add("female");
+    }
+
+    if (parseInt(age.innerText, 10) < 18) {
+      row.style.textDecoration = "line-through";
+    }
+  }
 }
