bf11bd51ac3ddc1e6e460956e176b25da86dc59a
Andrey Kozhuhov <andrpre96@gmail.com>
2024-11-10 22:05:37 +0300

    5 lesson / 1,2,3 task: solved

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,31 @@
 function initCarousel() {
-  // ваш код...
+  const carousel = document.querySelector(".carousel");
+  const carouselInner = carousel.querySelector(".carousel__inner");
+  const slides = carouselInner.querySelectorAll(".carousel__slide");
+  const arrowRight = carousel.querySelector(".carousel__arrow_right");
+  const arrowLeft = carousel.querySelector(".carousel__arrow_left");
+
+  let currentSlide = 0;
+  const totalSlides = slides.length;
+  const slideWidth = carouselInner.offsetWidth;
+
+  function updateArrows() {
+    arrowLeft.style.display = currentSlide === 0 ? "none" : "";
+    arrowRight.style.display = currentSlide === totalSlides - 1 ? "none" : "";
+  }
+
+  function moveSlide(direction) {
+    currentSlide += direction;
+    carouselInner.style.transform = `translateX(-${
+      currentSlide * slideWidth
+    }px)`;
+    updateArrows();
+  }
+
+  arrowRight.addEventListener("click", () => moveSlide(1));
+  arrowLeft.addEventListener("click", () => moveSlide(-1));
+
+  updateArrows();
 }
+
+initCarousel();
