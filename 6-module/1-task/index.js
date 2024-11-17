8dedd36972b9dba64b7307278d43f55ef43e81f1
Andrpre <andrpre96@gmail.com>
2024-11-17 17:55:51 +0300

    6 lesson / 1,2,3 task: solved

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,52 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.elem = this.createTable();
+  }
+
+  createTable() {
+    const table = document.createElement("table");
+    table.append(this.createHeader(), this.createBody());
+    return table;
+  }
+
+  createHeader() {
+    const thead = document.createElement("thead");
+    thead.innerHTML = `
+        <tr>
+          <th>Имя</th>
+          <th>Возраст</th>
+          <th>Зарплата</th>
+          <th>Город</th>
+          <th></th>
+        </tr>
+      `;
+    return thead;
+  }
+
+  createBody() {
+    const tbody = document.createElement("tbody");
+    this.rows.forEach((row) => tbody.append(this.createRow(row)));
+    return tbody;
+  }
+
+  createRow(row) {
+    const tr = document.createElement("tr");
+    tr.innerHTML = `
+        <td>${row.name}</td>
+        <td>${row.age}</td>
+        <td>${row.salary}</td>
+        <td>${row.city}</td>
+        <td><button>X</button></td>
+      `;
+
+    this.addRemoveButtonListener(tr);
+    return tr;
+  }
+
+  addRemoveButtonListener(rowElement) {
+    const button = rowElement.querySelector("button");
+    button.addEventListener("click", () => rowElement.remove());
   }
 }
--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,4 +1,58 @@
+import createElement from "../../assets/lib/create-element.js";
+
 export default class ProductCard {
   constructor(product) {
+    this.product = product;
+    this.elem = this.createCard();
+  }
+
+  createCard() {
+    const card = createElement(`
+      <div class="card">
+        ${this.createCardTop()}
+        ${this.createCardBody()}
+      </div>
+    `);
+
+    this.initEventListeners(card);
+
+    return card;
+  }
+
+  createCardTop() {
+    return `
+      <div class="card__top">
+        <img src="/assets/images/products/${
+          this.product.image
+        }" class="card__image" alt="product">
+        <span class="card__price">€${this.product.price.toFixed(2)}</span>
+      </div>
+    `;
+  }
+
+  createCardBody() {
+    return `
+      <div class="card__body">
+        <div class="card__title">${this.product.name}</div>
+        <button type="button" class="card__button">
+          <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+        </button>
+      </div>
+    `;
+  }
+
+  initEventListeners(card) {
+    const cardButton = card.querySelector(".card__button");
+
+    cardButton.addEventListener("click", () => this.onProductAdd(card));
+  }
+
+  onProductAdd(card) {
+    const productAddEvent = new CustomEvent("product-add", {
+      detail: this.product.id,
+      bubbles: true,
+    });
+
+    card.dispatchEvent(productAddEvent);
   }
 }
